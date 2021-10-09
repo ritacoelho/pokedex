2c4a94113d3257119ccf91c5bb9e4e286ee524ad
ritacoelho <rita.coelho@baseform.com>
2021-10-09 22:28:40 +0100

    initializing project

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+import HtmlWebPackPlugin from "html-webpack-plugin";
+const htmlPlugin = new HtmlWebPackPlugin({
+ template: "./src/index.html",
+ filename: "./index.html"
+});
+export const mode = 'development';
+export const module = {
+    rules: [{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader"
+        }
+    },
+    {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+    }
+    ]
+};
+export const plugins = [htmlPlugin];
